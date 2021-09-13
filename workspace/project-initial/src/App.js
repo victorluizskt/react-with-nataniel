50f19e71bb24b00cf4b6b357f4c59947f35b6d86
Victor Luiz <victorluiz@MacBook-de-Victor.local>
2021-09-13 08:46:18 -0300

    Alter state with useState

--- workspace/project-initial/src/App.js
+++ workspace/project-initial/src/App.js
@@ -3,13 +3,28 @@ import './App.css';
 import Header from './component/Header';
 import User from './component/User';
 import Photo from './assets/profile.png';
+import { useState } from 'react';
 
 function App() {
+
+  const [name, setName] = useState("Victor");
+  const changeState = () => {
+    if(name === "Victor"){
+      setName("Victor Luiz")
+    } else {
+      setName("Victor")
+    }
+  }
   return (
     <>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+      <div> {name} </div>
+      <button 
+        onClick={changeState}> 
+        Alter state 
+      </button>
       <User 
         name="Victor Luiz"
         picture={Photo}
