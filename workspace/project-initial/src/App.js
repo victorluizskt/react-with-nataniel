15f0e98542bbf892f32779ea7e433abaa1fb5244
Victor Luiz <victorluiz@MacBook-de-Victor.local>
2021-09-14 07:48:34 -0300

    Forms and events

--- workspace/project-initial/src/App.js
+++ workspace/project-initial/src/App.js
@@ -7,20 +7,35 @@ import { useState } from 'react';
 
 function App() {
 
-  const [name, setName] = useState("Victor");
+  const [person, setPerson] = useState({name: 'Victor Luiz', email: 'victorluizcefet@gmail.com'});
   const changeState = () => {
-    if(name === "Victor"){
-      setName("Victor Luiz")
+    if(person.name === "Victor"){
+      setPerson({name: 'Victor Luiz'})
     } else {
-      setName("Victor")
+      setPerson({name: 'Victor'})
     }
   }
+
+  const changeInputName = event => {
+    let target = event.target;
+    setPerson({name: target.value})
+  }
+
+  const changeInputEmail = event => {
+    let target = event.target;
+    setPerson({email: target.value})
+  }
+
   return (
     <>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      <div> {name} </div>
+      <img src={logo} className="App-logo" alt="logo" />
+      <form> 
+        <input type="text" value={person.name} onChange={event => changeInputName(event)}/>
+        <input type="text" name="name" value={person.email} onChange={event => changeInputEmail(event)}/>
+      </form>
+      <div> {person.name} - {person.email} </div>
       <button 
         onClick={changeState}> 
         Alter state 
